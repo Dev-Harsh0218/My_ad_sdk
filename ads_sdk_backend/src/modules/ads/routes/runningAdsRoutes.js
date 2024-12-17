fbe1685b80479e83ea59702b1df61c90e98084c6
Harsh Bhardwaj <harsh.bhardwaj@appsuccessor.com>
2024-12-17 17:16:02 +0530

    last changes on windows

--- ads_sdk_backend/src/modules/ads/routes/runningAdsRoutes.js
+++ ads_sdk_backend/src/modules/ads/routes/runningAdsRoutes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const runningAdsController = require('../controllers/runningAdsController');
 
 // routes for running ads controller are here
+//creating a single running ad
 router.post('/create-running-ad',runningAdsController.createRunningAd);
+// creating multiple running ads
 router.post('/create-running-multiple-ads',runningAdsController.createMultipleRunnningAds);
+//getting all running ads
 router.get('/get-all-running-ads',runningAdsController.getAllRunningAds);
+//deleting a running ad
+// router.delete('/delete-running-ad/:id',runningAdsController.deleteRunningAd);
 
 module.exports = router;
